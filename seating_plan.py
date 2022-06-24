6e78a7cdac1b6589e084d5d328688140704d6586
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2022-06-24 21:22:06 +0800

    Replace hard-coded state space with input state space

--- seating_plan.py
+++ seating_plan.py
@@ -1,27 +1,33 @@
+import string
+import random
+
 class Node:
-  def __init__(self, state=None, parent=None, cost=0):
+  def __init__(self, state=None, parent=None, parent_list=None, cost=0):
     self.state = state
     self.parent = parent
+    self.parent_list = parent_list
     self.children = []
     self.cost = cost
 
   def addChildren(self, children):
     self.children.extend(children)
 
-def expandAndReturnChildren(state_space, node):
+def expandAndReturnChildren(state_space, explored, node):
   children = []
   # ['A', 'B', 1.5]
   print("State space: " + str(state_space))
   print("Node state: " + str(node.state))
+  parent_list = get_parent_list(node, explored)
+  print("Node Parents: " + str(parent_list))
   for [m,n,c] in state_space:
     if m == node.state:
       # print (['A', n, c])
       # print("Found m = node.state")
-      children.append(Node(n, node.state, node.cost+c))
+      children.append(Node(n, node.state, parent_list, node.cost+c))
     elif n == node.state:
       # print ([m, 'A', c])
       # print("Found n = node.state")
-      children.append(Node(m, node.state, node.cost+c))
+      children.append(Node(m, node.state, parent_list, node.cost+c))
   print("Expand and return children:" + str(children))
   return children
 
@@ -75,7 +81,50 @@ def get_parent_count(node, explored):
         break
   return parent_count
 
-def ucs(state_space, initial_state, total_persons):
+def input_no_of_persons():
+  no_of_persons = int(input('Enter the number of people to be seated: '))
+  return no_of_persons
+
+def input_state_space(no_of_persons):
+  alp_list = list(string.ascii_uppercase)
+  # Represent N number of people with the first N alphabet
+  person_list = alp_list [:no_of_persons]
+  
+  # Define initial state
+  first_person = random.choice(person_list)
+  initial_state = first_person
+  print("Initial State: " + str(initial_state))
+
+  print('Please enter two uncomfort value for each pair of people.\n')
+  print('The uncomfort value range from 1 (most comfortable) to 5 (most uncomfortable)\n')
+  print('eg: 1,2 ') 
+    
+  # Show all possible pairs from the list
+  pair = []
+  person_pair = [] 
+  for i in range (len(person_list)-1):
+      j = i + 1
+      while j < (len(person_list)):
+          pair=[person_list[i],person_list[j]]
+          person_pair.append(pair)
+          j += 1
+          
+  # Ask the user to input the uncomfort value for each pair of people
+  uncomfort_val = []
+  state_space = []
+  for i in range (len(person_pair)):
+      print('Enter the uncomfort value of', person_pair[i][0], 'and', person_pair[i][1],':')
+      x,y = input().split(",")
+      
+      # Standardise the uncomfort value to double
+      avg_val = float((int(x) + int(y)) / 2)
+      uncomfort_val=[person_pair[i][0],person_pair[i][1]]
+      uncomfort_val.append(avg_val)
+      state_space.append(uncomfort_val)
+      
+  return [state_space, initial_state]
+
+def ucs(state_space, initial_state, no_of_persons):
   frontier = []
   explored = []
   found_goal = False
@@ -83,19 +132,19 @@ def ucs(state_space, initial_state, total_persons):
   solution = []
   # add initial state to frontier
   frontier.append(Node(initial_state, None))
-  depth = 0
-  # parent_count = 0
+  # depth = 0
+  parent_count = 0
   
   while not found_goal:
     # goal test at expansion
-    if depth == 3:
+    if parent_count == (no_of_persons - 1):
       found_goal = True
       print("All persons have been allocated to a seat!")
       print("Allocated persons: " + str(explored))
       goalie = frontier[0]
       break
     # expand the first in the frontier
-    children = expandAndReturnChildren(state_space, frontier[0])
+    children = expandAndReturnChildren(state_space, explored, frontier[0])
     # add children list to the expanded node
     frontier[0].addChildren(children)
     # add to the explored list
@@ -109,10 +158,11 @@ def ucs(state_space, initial_state, total_persons):
       # if child.state in [e.state for e in explored]: 
       #   print("Current node is expanded previously.")    
       # if not (child.state in [e.state for e in explored]): 
-      #   print("Current node is not expanded previously.")       
-      frontier = appendAndSort(frontier, child, explored)
-    print("Frontier[0] Parent: " + str(frontier[0].parent))
-    print("Explored[-1] State: " + str(explored[-1].state))
+      #   print("Current node is not expanded previously.")
+      if not (child.state in [e.state for e in explored]) and not (child.parent_list == [e.parent_list for e in explored]):
+        frontier = appendAndSort(frontier, child, explored)
+    # print("Frontier[0] Parent: " + str(frontier[0].parent))
+    # print("Explored[-1] State: " + str(explored[-1].state))
     # if frontier[0].parent == explored[-1].state:
     #   print("Depth increased.")
     #   depth += 1
@@ -140,44 +190,12 @@ def ucs(state_space, initial_state, total_persons):
 
 if __name__ == "__main__":
   # state space and step cost definition
-  state_space = [
-    ['A', 'B', 1.5],
-    ['A', 'C', 2.0],
-    ['A', 'D', 2.0],
-    ['A', 'E', 4.0],
-    ['B', 'C', 1.0],
-    ['B', 'D', 4.5],
-    ['B', 'E', 2.5],
-    ['C', 'D', 3.0],
-    ['C', 'E', 1.5],
-    ['D', 'E', 1.0]
-  ]
-
-  # # Get a list of alphabet to represent the peoples
-  # alp_list = list(string.ascii_uppercase)
-  # n =  int(input('Enter the number of people to be seated: '))
-    
-  # person_list = alp_list [:n]
-  # print(person_list) 
-
-  # Define initial state
-  initial_state = 'A'
-  # first_person = random.choice(person_list)
-  # initial_state.append(first_person)  
-  print(initial_state)
-
-  # # Input 
-  # print('Please enter the uncomfort value\n')
-  # print('The uncomfort value range from 1 (most comfortable) to 5 (most uncomfortable)\n')
-  # for i in range (n):
-  #   for j in range (n):
-  #       if (person_list[i] != person_list[j]):
-  #           print('Enter the uncomfort value of', person_list[i], 'towards', person_list[j],':')
-  #           v = input()
-  #           uncomfortVal=[person_list[i],person_list[j]]
-  #           uncomfortVal.append(v)
-  #           state_space.append(uncomfortVal) 
-
-  [solution, cost] = ucs(state_space, initial_state, 5)
+  no_of_persons = input_no_of_persons()
+  [state_space, initial_state] = input_state_space(no_of_persons)
+  print("State space generated:")
+  for state in state_space:
+    print(state)
+  
+  [solution, cost] = ucs(state_space, initial_state, no_of_persons)
   print("Solution:", solution)
   print("Path Cost:", cost)
